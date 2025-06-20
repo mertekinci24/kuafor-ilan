8124c7624cdcc6ae992daef32284ea92eac2a7d7
mertekinci24 <130255156+mertekinci24@users.noreply.github.com>
2025-06-20 16:09:44 +0300

    Create models.py

---
+++ apps/core/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+
+class TimeStampedModel(models.Model):
+    """Abstract base model with created and updated timestamps"""
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+
+class SoftDeleteModel(models.Model):
+    """Abstract base model with soft delete functionality"""
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    
+    class Meta:
+        abstract = True
+    
+    def soft_delete(self):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
+    
+    def restore(self):
+        self.is_deleted = False
+        self.deleted_at = None
+        self.save()
+      
