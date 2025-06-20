c2a4957604a86b444597ebb200c9df4648a9fae6
mertekinci24 <130255156+mertekinci24@users.noreply.github.com>
2025-06-20 19:08:35 +0300

    Update models.py

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 
-# Authentication models will be added later
-# Currently using Django's default User model
+# Authentication models - temporarily disabled
+# Will be implemented later
