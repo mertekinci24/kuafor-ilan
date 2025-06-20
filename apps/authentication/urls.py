62da11aa370c25fe862c66c6af8593fdfc7226be
mertekinci24 <130255156+mertekinci24@users.noreply.github.com>
2025-06-20 16:12:02 +0300

    Create urls.py

---
+++ apps/authentication/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'authentication'
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/update/', views.update_profile, name='update_profile'),
+]
