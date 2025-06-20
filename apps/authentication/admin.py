ad7d37c393ce85a9723b065593bfd8f8350d497c
mertekinci24 <130255156+mertekinci24@users.noreply.github.com>
2025-06-20 19:11:59 +0300

    Update admin.py

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -1,26 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
 
-@admin.register(User)
-class UserAdmin(BaseUserAdmin):
-    """Custom User admin"""
-    list_display = [
-        'email', 'username', 'full_name', 'user_type', 
-        'is_email_verified', 'is_active', 'date_joined'
-    ]
-    list_filter = [
-        'user_type', 'is_email_verified', 'is_active', 'date_joined'
-    ]
-    search_fields = ['email', 'username', 'first_name', 'last_name']
-    ordering = ['-date_joined']
-    
-    fieldsets = BaseUserAdmin.fieldsets + (
-        ('Additional Info', {
-            'fields': (
-                'phone_number', 'user_type', 'avatar', 
-                'is_email_verified', 'is_profile_complete', 'last_active'
-            )
-        }),
-    )
-  
+# Authentication admin - temporarily disabled
+# Will be implemented later
