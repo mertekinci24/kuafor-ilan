8ee97a1307cd639fc0a00c91e23d518e3a147035
mertekinci24 <130255156+mertekinci24@users.noreply.github.com>
2025-06-21 12:27:22 +0300

    Update urls.py

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -6,4 +6,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', TemplateView.as_view(template_name='auth/register.html'), name='register'),
 ]
