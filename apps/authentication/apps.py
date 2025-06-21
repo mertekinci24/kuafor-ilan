378a22b7a02b1b2feda9d98864dd26a64501909e
mertekinci24 <130255156+mertekinci24@users.noreply.github.com>
2025-06-21 14:29:26 +0300

    Update apps.py

--- apps/authentication/apps.py
+++ apps/authentication/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
+
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.authentication'
-  
+    verbose_name = 'Kimlik Doğrulama'
+    
+    def ready(self):
+        import apps.authentication.signals
+        
