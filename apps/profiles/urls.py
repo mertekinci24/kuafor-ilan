c88239a71c11e2450ce83b50f141e0a9598f1d19
mertekinci24 <130255156+mertekinci24@users.noreply.github.com>
2025-06-21 09:49:27 +0300

    Update urls.py

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from . import views
 
 app_name = 'profiles'
 
 urlpatterns = [
-    # Profile URLs will be added here
+    path('', views.profile_view, name='profile'),  # Kendi profili
+    path('<int:user_id>/', views.profile_view, name='profile_detail'),  # Başka kullanıcı profili
+    path('edit/', views.profile_edit_view, name='profile_edit'),  # Profil düzenleme
+    path('applications/', views.my_applications_view, name='my_applications'),  # Başvurularım
+    path('jobs/', views.my_jobs_view, name='my_jobs'),  # İlanlarım
 ]
