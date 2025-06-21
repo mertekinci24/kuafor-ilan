77fc2af14ff2de273232713ac6c38111918f63ee
mertekinci24 <130255156+mertekinci24@users.noreply.github.com>
2025-06-21 03:59:16 +0300

    Update admin.py

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Authentication admin - temporarily disabled
-# Will be implemented later
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'user_type', 'is_email_verified', 'date_joined')
+    list_filter = ('user_type', 'is_email_verified', 'is_active')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Ek Bilgiler', {'fields': ('user_type', 'phone', 'is_phone_verified', 'is_email_verified')}),
+    )
+  
