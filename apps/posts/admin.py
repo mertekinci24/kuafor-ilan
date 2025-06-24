f6f60ca36983ef6159ae0b00d23889ec81d581f9
mertekinci24 <130255156+mertekinci24@users.noreply.github.com>
2025-06-24 21:12:25 +0300

    Update admin.py

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.utils.html import format_html
 from .models import PostCategory, Post, PostLike, PostComment, PostSave
 
-
 @admin.register(PostCategory)
 class PostCategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'icon', 'color_display', 'is_active', 'created_at']
@@ -16,20 +15,6 @@ class PostCategoryAdmin(admin.ModelAdmin):
             obj.color
         )
     color_display.short_description = 'Renk'
-
-
-class PostLikeInline(admin.TabularInline):
-    model = PostLike
-    extra = 0
-    readonly_fields = ['user', 'created_at']
-
-
-class PostCommentInline(admin.TabularInline):
-    model = PostComment
-    extra = 0
-    readonly_fields = ['author', 'created_at']
-    fields = ['author', 'content', 'parent', 'is_deleted', 'created_at']
-
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -46,39 +31,10 @@ class PostAdmin(admin.ModelAdmin):
         'id', 'likes_count', 'comments_count', 'shares_count', 
         'views_count', 'created_at', 'updated_at'
     ]
-    inlines = [PostLikeInline, PostCommentInline]
-    
-    fieldsets = (
-        ('Temel Bilgiler', {
-            'fields': ('id', 'author', 'post_type', 'title', 'content')
-        }),
-        ('Medya', {
-            'fields': ('image', 'video'),
-            'classes': ('collapse',)
-        }),
-        ('Organizasyon', {
-            'fields': ('category', 'tags', 'location', 'visibility')
-        }),
-        ('Ayarlar', {
-            'fields': ('allow_comments', 'is_pinned', 'is_featured')
-        }),
-        ('Moderasyon', {
-            'fields': ('is_published', 'is_approved', 'is_deleted')
-        }),
-        ('İstatistikler', {
-            'fields': ('likes_count', 'comments_count', 'shares_count', 'views_count'),
-            'classes': ('collapse',)
-        }),
-        ('Zaman Bilgileri', {
-            'fields': ('created_at', 'updated_at'),
-            'classes': ('collapse',)
-        }),
-    )
     
     def content_preview(self, obj):
         return obj.content[:100] + '...' if len(obj.content) > 100 else obj.content
     content_preview.short_description = 'İçerik Önizleme'
-
 
 @admin.register(PostComment)
 class PostCommentAdmin(admin.ModelAdmin):
@@ -91,13 +47,11 @@ class PostCommentAdmin(admin.ModelAdmin):
         return obj.content[:50] + '...' if len(obj.content) > 50 else obj.content
     content_preview.short_description = 'Yorum'
 
-
 @admin.register(PostLike)
 class PostLikeAdmin(admin.ModelAdmin):
     list_display = ['post', 'user', 'created_at']
     list_filter = ['created_at']
     search_fields = ['user__first_name', 'user__email', 'post__content']
-
 
 @admin.register(PostSave)
 class PostSaveAdmin(admin.ModelAdmin):
