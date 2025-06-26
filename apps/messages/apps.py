19e457fbd0b2e52d993ef262d19bd241f4f0f5d3
mertekinci24 <130255156+mertekinci24@users.noreply.github.com>
2025-06-26 09:13:43 +0300

    Update apps.py

--- apps/messages/apps.py
+++ apps/messages/apps.py
@@ -1,1 +1,12 @@
+from django.apps import AppConfig
 
+
+class MessagesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.messages'
+    verbose_name = 'Mesajlaşma'
+    
+    def ready(self):
+        """App hazır olduğunda çalışacak kod"""
+        import apps.messages.signals
+      
