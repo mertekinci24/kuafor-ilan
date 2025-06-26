5531396c18037988ac023aa78c69b7830a949129
mertekinci24 <130255156+mertekinci24@users.noreply.github.com>
2025-06-26 09:33:42 +0300

    Update apps.py

--- apps/notifications/apps.py
+++ apps/notifications/apps.py
@@ -1,1 +1,12 @@
+from django.apps import AppConfig
 
+
+class NotificationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.notifications'
+    verbose_name = 'Bildirimler'
+    
+    def ready(self):
+        """App hazır olduğunda çalışacak kod"""
+        import apps.notifications.signals
+      
