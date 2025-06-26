31d93b1e8392a5dbf59179ae30c11366334663c3
mertekinci24 <130255156+mertekinci24@users.noreply.github.com>
2025-06-26 09:43:31 +0300

    Update apps.py

--- apps/messages/apps.py
+++ apps/messages/apps.py
@@ -5,8 +5,9 @@ class MessagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.messages'
     verbose_name = 'Mesajlaşma'
+    label = 'messaging'  # Çakışmayı önlemek için özel etiket
     
     def ready(self):
         """App hazır olduğunda çalışacak kod"""
         import apps.messages.signals
-      
+        
