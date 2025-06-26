0024dfc1da260d79d4ce4f5afa17b59d72f64d4e
mertekinci24 <130255156+mertekinci24@users.noreply.github.com>
2025-06-26 14:39:34 +0300

    Update 0001_initial.py

--- apps/posts/migrations/0001_initial.py
+++ apps/posts/migrations/0001_initial.py
@@ -63,5 +63,19 @@ class Migration(migrations.Migration):
                 'ordering': ['-created_at'],
             },
         ),
+        migrations.CreateModel(
+            name='PostShare',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='shares', to='posts.post')),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='shared_posts', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name': 'Gönderi Paylaşımı',
+                'verbose_name_plural': 'Gönderi Paylaşımları',
+                'unique_together': {('user', 'post')},
+            },
+        ),
     ]
     
