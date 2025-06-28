2d2e789d0030ec670b918331307aa72f26c764f1
Mert EKİNCİ <merteekinci@gmail.com>
2025-06-28 03:02:06 +0300

    Fix: Update LOGIN_URL to /auth/login/

--- kuafor_ilan/settings.py
+++ kuafor_ilan/settings.py
@@ -128,9 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Login ve Logout yönlendirmeleri (Mevcut)
-LOGIN_URL = '/login/'
+LOGIN_URL = '/auth/login/'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/auth/login/'
 
 
 # Email Ayarları (Mevcut)
